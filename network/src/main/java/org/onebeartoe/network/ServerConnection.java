f0a8324276926391c843cfa44ef3fc4a3bf32874
Roberto Marquez <onebeartoe@gmail.com>
2014-10-01 13:43:17 -0600

    A method to send static (CSS/Javascript) files was added.

--- network/src/main/java/org/onebeartoe/network/ServerConnection.java
+++ network/src/main/java/org/onebeartoe/network/ServerConnection.java
@@ -1,10 +1,12 @@
 package org.onebeartoe.network;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import org.onebeartoe.io.TextFileReader;
 
 /**
  * @author Roberto Marquez
@@ -14,6 +16,8 @@ public abstract class ServerConnection implements Runnable, Cloneable
 
     protected Socket client;
 
+    protected final String path = "/onebeartoe/juke/ui/";
+    
     /**
      * @param client the client to set
      */
@@ -35,25 +39,33 @@ public abstract class ServerConnection implements Runnable, Cloneable
         return obj;
     }
 
-    protected void sendHttpResponse(String html, boolean includeHeader) throws IOException
+    protected void sendHttpResponse(String html, boolean includeHeaders) throws IOException
     {
-        String heaersMessage = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n";
-        heaersMessage += "\r\n";
+        String headers = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n";
+        headers += "\r\n";
 
         OutputStream outs = client.getOutputStream();
         PrintWriter out = new PrintWriter(new OutputStreamWriter(outs, "UTF-8"), true);
-        if (includeHeader)
+        if (includeHeaders)
         {
-            out.print(heaersMessage);
+            out.print(headers);
         }
         out.println(html);
         out.flush();
         out.close();
     }
+    
+    
+    protected void sendNonHtmlResponse(String filename) throws IOException
+    {
+        InputStream instream = getClass().getResourceAsStream(path + filename);
+        String html = TextFileReader.readText(instream);
+        boolean includeHeader = false;
+        sendHttpResponse(html, includeHeader);        
+    }
 
     protected String invalidRequest(String request)
     {
-
         StringBuilder out = new StringBuilder();
         out.append("Request Recieved Busta: " + request);
         out.append("<br /><br />" + getClass().getSimpleName() + " accepts requests like \"GET filename.html\"<br />");
