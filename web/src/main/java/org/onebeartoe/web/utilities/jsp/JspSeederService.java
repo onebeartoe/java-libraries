cc23a53dc7e3b1155912c63cc9e73647da23b30f
onebeartoe <onebeartoe@gmail.com>
2016-09-24 10:02:50 -0500

    This is the initial commit for a service that generates JSP tempate content on the filesytem.

---
+++ web/src/main/java/org/onebeartoe/web/utilities/jsp/JspSeederService.java
@@ -0,0 +1,11 @@
+
+package org.onebeartoe.web.utilities.jsp;
+
+/**
+ *
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ */
+public interface JspSeederService 
+{
+    public boolean indexSeed();
+}
