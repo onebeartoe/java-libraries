00145ef1300efc0ba242e9f2849ce8e1e8926aa9
onebeartoe <onebeartoe@gmail.com>
2016-09-24 11:27:27 -0500

    A JUnit class was added to test the output of the JSP seeder service.

--- web/src/main/java/org/onebeartoe/web/utilities/jsp/JspSeederService.java
+++ web/src/main/java/org/onebeartoe/web/utilities/jsp/JspSeederService.java
@@ -3,6 +3,10 @@ package org.onebeartoe.web.utilities.jsp;
 
 /**
  *
+ * Given the filesystem location of a JEE Web directory and an accessible (target) 
+ * directoy, create supporting index.jsp, WEB-INF/jsp/.../bottom.jsp, 
+ * WEB-INF/jsp/.../index.jsp, and properties.jsp
+ * 
  * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
  */
 public interface JspSeederService 
---
+++ web/src/main/java/org/onebeartoe/web/utilities/jsp/StreamedJspSeederService.java
@@ -0,0 +1,22 @@
+
+package org.onebeartoe.web.utilities.jsp;
+
+/**
+ *
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ * 
+ */
+public class StreamedJspSeederService implements JspSeederService
+{
+
+    @Override
+    public boolean indexSeed() 
+    {
+        boolean indexed = 
+                
+        
+                
+        return indexed;
+    }
+    
+}
