dd26fb8b8427b2421d03ab9547adfb44eb83a665
Roberto Marquez <onebeartoe@gmail.com>
2017-04-13 11:05:55 -0500

    The stack trace is now printed on this error.

--- onebeartoe-system/src/main/java/org/onebeartoe/io/TextFileWriter.java
+++ onebeartoe-system/src/main/java/org/onebeartoe/io/TextFileWriter.java
@@ -12,26 +12,25 @@ import java.io.PrintWriter;
  */
 public class TextFileWriter implements TextWriter
 {
-
     /**
      * By default this method writes the test to the beginning of the file. Any
      * text already in the file is overwritten.
      */
-    public boolean writeText(File f, String text)
+    public boolean writeText(File outfile, String text)
     {
-        return writeText(f, text, false);
+        return writeText(outfile, text, false);
     }
 
     /**
      * If the value of append is true the text will be add to the end of the
      * file. if the value is false the text is add the begining of the file.
      */
-    public boolean writeText(File f, String text, boolean append)
+    public boolean writeText(File outfile, String text, boolean append)
     {
         boolean saved = true;
         try
         {
-            FileWriter file = new FileWriter(f, append);
+            FileWriter file = new FileWriter(outfile, append);
             PrintWriter pic_index = new PrintWriter(file);
             pic_index.println(text);
             pic_index.close();
@@ -39,6 +38,7 @@ public class TextFileWriter implements TextWriter
         catch(FileNotFoundException fnfe)
         {
             saved = false;
+            fnfe.printStackTrace();
         }
         catch (IOException ioe)
         {
