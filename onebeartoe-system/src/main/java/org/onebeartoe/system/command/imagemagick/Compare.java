8dada0e5644d36d51f527d527ef22df63d042b46
onebeartoe <onebeartoe@gmail.com>
2017-08-13 11:59:36 -0500

    The percetnage was moved to an integer value.

--- onebeartoe-system/src/main/java/org/onebeartoe/system/command/imagemagick/Compare.java
+++ onebeartoe-system/src/main/java/org/onebeartoe/system/command/imagemagick/Compare.java
@@ -48,6 +48,9 @@ public class Compare extends SystemCommand
         
         float f = Float.valueOf(s);
         
+        // move it to an integer percetage
+        f = f * 100.0f;
+        
         int i = (int) f;
         
         results.exitCode = i;
