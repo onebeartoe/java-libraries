97bf590c6eaa32cebb770fa883facb2bb23baf85
Roberto Marquez <onebeartoe@gmail.com>
2017-09-25 10:22:15 -0500

    A documen change listener was added and the text is now available via a getter method.

--- onebeartoe-application/src/main/java/org/onebeartoe/application/ui/swing/ScrollableTextArea.java
+++ onebeartoe-application/src/main/java/org/onebeartoe/application/ui/swing/ScrollableTextArea.java
@@ -6,69 +6,80 @@ import java.awt.Font;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.event.DocumentListener;
 
 public class ScrollableTextArea extends JScrollPane 
 {
+    static final long serialVersionUID = 3L;
 
-	static final long serialVersionUID = 3L;
+    private Font font;
 
-	private Font font;
+    private JTextArea text;
 
-	private JTextArea text;
+    private Color textareaForeground;
+    
+    private Color textareaBackground;
 
-	private Color textareaForeground;
-	private Color textareaBackground;
+    public ScrollableTextArea(String default_str)
+    {
+        text = new JTextArea(default_str);
+        setViewportView(text);
+    }
+    
+    public void addTextListener(DocumentListener listener)
+    {
+        text.getDocument()
+            .addDocumentListener(listener);
+    }
+    
+    public String getText()
+    {
+        return text.getText();
+    }
 
-	public ScrollableTextArea(String default_str) 
-	{
-		text = new JTextArea(default_str);
-		setViewportView(text);
-	}
-	
-	public void setLineWrap(boolean wrapped)
-	{
-		text.setLineWrap(wrapped);
-	}
+    public void setLineWrap(boolean wrapped)
+    {
+            text.setLineWrap(wrapped);
+    }
 
-	public void setText(String str) 
-	{
-		
-		text.setText(str);
-	}
+    public void setText(String str) 
+    {
 
-	public void appendText(String str) {
-		text.append(str);
-	}
-	
-	public void setTextAreaFont(Font f) {
-		if(f != null) {
-			text.setFont(f);
-		}
-	}
-	
-	
+            text.setText(str);
+    }
 
-	public Font getFont() {
-		return font;
-	}
+    public void appendText(String str) 
+    {
+            text.append(str);
+    }
 
-	public Color getTextareaForeground() {
-		return textareaForeground;
-	}
+    public void setTextAreaFont(Font f) 
+    {
+            if(f != null) {
+                    text.setFont(f);
+            }
+    }
 
-	public void setTextareaForeground(Color textareaForeground) 
-	{
-		text.setForeground(textareaForeground);
-	}
+    public Font getFont() 
+    {
+            return font;
+    }
 
-	public Color getTextareaBackground() {
-		return textareaBackground;
-	}
+    public Color getTextareaForeground() {
+            return textareaForeground;
+    }
 
-	public void setTextareaBackground(Color textareaBackground) 
-	{
-		text.setBackground(textareaBackground);
-//		text.setText("chagned to back gahorund");
-	}
-	
+    public void setTextareaForeground(Color textareaForeground) 
+    {
+            text.setForeground(textareaForeground);
+    }
+
+    public Color getTextareaBackground() {
+            return textareaBackground;
+    }
+
+    public void setTextareaBackground(Color textareaBackground) 
+    {
+            text.setBackground(textareaBackground);
+    }	
 }
