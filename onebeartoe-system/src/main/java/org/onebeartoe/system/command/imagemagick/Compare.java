ccfe33f79900886c3a39e1588040ac49c84459c8
Roberto Marquez <onebeartoe@gmail.com>
2017-09-27 10:47:21 -0500

    The error handling was updated a little bit.

--- onebeartoe-system/src/main/java/org/onebeartoe/system/command/imagemagick/Compare.java
+++ onebeartoe-system/src/main/java/org/onebeartoe/system/command/imagemagick/Compare.java
@@ -4,7 +4,8 @@ package org.onebeartoe.system.command.imagemagick;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
-import org.onebeartoe.system.CommandResults;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.onebeartoe.system.command.SystemCommand;
 import org.onebeartoe.system.command.SystemCommandProfile;
 
@@ -21,9 +22,14 @@ public class Compare extends SystemCommand
     private final static double DEFAULT_ERROR_THRESHOLD = 2.5;
     
     private double errorThreshold;
+    
+    private Logger logger;
 
     public Compare(String path1, String path2, double errorThreshold)
     {
+        String name = getClass().getName(); 
+        logger = Logger.getLogger(name);
+        
         this.errorThreshold = errorThreshold;
         profile = new SystemCommandProfile();
         
@@ -48,13 +54,22 @@ public class Compare extends SystemCommand
      * Any comparison difference (the value given between the parenthesis by the
      * compare command) greater than this value is treated as an error.
      */
-        @Override
+    @Override
     protected String processStdErr(InputStream is)
     {
         String stderr = super.processStdErr(is);
         
         int begin = stderr.lastIndexOf("(") + 1;
         int end  =  stderr.lastIndexOf(")");
+        
+        if(begin < 0 || end < 0)
+        {
+            String message = "The substring look up for the comparison percentage failed.  " +
+                                "Is the ImageMagic command available?";
+            
+            message += "\n\n" + stderr;
+            logger.log(Level.SEVERE, message);
+        }
         
         String s = stderr.substring(begin, end);
         
