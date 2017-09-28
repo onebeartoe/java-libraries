8f8cf1ab9b1070aa0c721d4f2d0def30fa62ac6d
roberto <onebeartoe@gmail.com>
2017-09-28 07:13:58 -0500

    This was updated to for use with Web sites.

---
+++ onebeartoe-application/src/main/java/org/onebeartoe/html/CodeGenerator.java
@@ -0,0 +1,78 @@
+/*
+ */
+package org.onebeartoe.html;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class CodeGenerator
+{
+    public String htmlBottom() 
+    {
+        return "\n</BODY>\n</HTML>";
+    }
+    
+    /**
+            This version takes the URL and returns an anchor tag
+            with the url as the text to click on
+    */	
+    public String getLinkTag( String url ) 
+    {
+        return getLinkTag(url, url);
+    }
+    /**
+            This version takes the URL and the text to click on,
+            then returns an anchor tag.
+    */	
+    public String getLinkTag( String url, String text ) 
+    {
+        AnchorTag tag = new AnchorTag(url, text);
+
+        return tag.toHtml();
+    }    
+    
+    /**
+     * @param content Takes the content of the page as a String
+     * @return Returns an HTML document with a "no title".
+    */
+    public String htmlify(String content) 
+    {
+        StringBuilder html = new StringBuilder( htmlTop("no title") );
+        html.append( content );
+        html.append( htmlBottom() );
+        
+        return html.toString();
+    }
+
+    /**
+            Takes the title of the page and the content of the page as Strings.
+            Returns an HTML document.
+    */
+    public String htmlify(String title, String content) 
+    {
+        StringBuffer html = new StringBuffer( htmlTop(title) );
+        html.append( content );
+        html.append( htmlBottom() );
+        return html.toString();
+    }
+
+    /** 
+     * @return a string containing just the html open tag
+     */
+    public String htmlTop() 
+    {
+            return("<html>\n");
+    }    
+    
+    /**
+     * 
+     * @param title the value used in the title tag
+     * @return a string with the value of an HTML document includingthe open html tag, a complete 
+     * head tag with title, and an open body tag.
+     */
+    public String htmlTop(String title) 
+    {
+            return("<html>\n<head><title>" + title + "</title></head>\n<body>");
+    }
+}
---
+++ onebeartoe-application/src/main/java/org/onebeartoe/html/pages/Menu.java
@@ -0,0 +1,100 @@
+
+package org.onebeartoe.html.pages;
+
+import java.util.HashMap;
+import java.util.Set;
+import org.onebeartoe.html.CodeGenerator;
+
+public class Menu 
+{
+    long id;
+
+    String title;
+
+    /**
+     * the keys are the label on the menu, the map values are the Page link
+     */
+    HashMap<String, String> options;
+    
+    private CodeGenerator codeGenerator;
+
+    public Menu()
+    {
+            id = -1;
+            title = "";
+            options = new HashMap<String, String>();
+            
+            codeGenerator = new CodeGenerator();
+    }
+	
+	public Menu(String title, HashMap<String, String> options)
+	{
+		id = -1;
+		this.title = title;
+		this.options = options;
+	}
+	
+	public void addOption(String label, String link)
+	{
+		options.put(label, link);
+	}
+	
+	public String toString()
+	{
+		StringBuffer buf = new StringBuffer();
+		buf.append("<div class=\"menu\">" + "\n");
+		buf.append("\t<h3>" + title + "</h3>" + "\n");
+		
+		Set<String> keys = options.keySet();
+		String label, link;
+		for(String key : keys)
+		{
+			label = key;
+			link = options.get(key);			
+			String anchorTag = codeGenerator.getLinkTag(link, label);		
+			buf.append("\t\t" + anchorTag  + "\n");
+		}
+				
+		buf.append("</div>"  + "\n\n");
+		
+		return buf.toString();
+	}
+
+	
+	public String getTitle() 
+	{
+		return title;
+	}
+
+	
+	public void setTitle(String title) 
+	{
+		this.title = title;
+	}
+
+	public HashMap<String, String> getOptions() 
+	{
+		return options;
+	}
+
+    public void setOptions(HashMap<String, String> options) 
+    {
+            this.options = options;
+    }
+
+    /**
+     * 
+     * @return a negative value for objects that have not been persisted, otherwise the primary key of this 
+     * object in the database.
+     */
+    public long getId() 
+    {
+            return id;
+    }
+
+    public void setId(long id) 
+    {
+            this.id = id;
+    }
+}
+  
