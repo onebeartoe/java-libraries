4c4fa4e39f897913d2cee6368168a1e98fe66f31
roberto <onebeartoe@gmail.com>
2017-09-28 06:27:17 -0500

    This page was added to represent a page on onebeartoe.com

---
+++ onebeartoe-application/src/main/java/org/onebeartoe/html/pages/OneWebPage.java
@@ -0,0 +1,30 @@
+
+package org.onebeartoe.html.pages;
+
+import java.awt.Menu;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Roberto Marquez
+ */
+public class OneWebPage
+{
+    /**
+     * This is a list of HTML elements that make up the main content of 
+     * a oneweb page.
+     */
+    public List<Object> content;
+    
+    /**
+     * These are all the navigation menus.
+     */
+    public List<Menu> menus;
+    
+    public OneWebPage()
+    {
+        content = new ArrayList();
+        
+        menus = new ArrayList();
+    }
+}
