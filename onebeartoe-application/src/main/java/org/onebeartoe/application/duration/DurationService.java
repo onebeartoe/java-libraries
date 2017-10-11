db1e7ee0e864afdd9ce091dd70952b68f672ea93
roberto <onebeartoe@gmail.com>
2017-10-11 13:58:04 -0500

    A duration service was added.

---
+++ onebeartoe-application/src/main/java/org/onebeartoe/application/duration/DurationService.java
@@ -0,0 +1,23 @@
+
+package org.onebeartoe.application.duration;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class DurationService
+{
+    public String durationMessage(Instant start, Instant end)
+    {
+        ChronoUnit units = ChronoUnit.SECONDS;
+        long duration = units.between(start,end);
+        long minutes = duration / 60;
+        long seconds = duration % 60;
+        String message = "The test suite ran " + minutes + " minutes " + seconds + " seconds.";
+                
+        return message;
+    }    
+}
