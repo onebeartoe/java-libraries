3bdbbe251a27106191291112a3a9e2257c1b0e52
Roberto Marquez <onebeartoe@users.noreply.github.com>
2018-05-14 15:30:56 -0500

    This is to address the 'interruptedException should not be ignored' item reported by SonarQube.

--- onebeartoe-system/src/main/java/org/onebeartoe/system/Sleeper.java
+++ onebeartoe-system/src/main/java/org/onebeartoe/system/Sleeper.java
@@ -15,6 +15,8 @@ public class Sleeper
         catch (InterruptedException ex) 
         {
             System.err.println("could not sleep");
+            
+            Thread.currentThread().interrupt();
         }                
     }
 }
