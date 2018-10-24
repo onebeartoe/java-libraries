2040f5fd3093f67298f1a54ca458acba495a5c01
Roberto Marquez <onebeartoe@gmail.com>
2018-10-24 14:49:07 -0500

    More tests were added.

---
+++ onebeartoe-multimedia/src/test/java/org/onebeartoe/multimedia/RegularCurrentSongServiceSpecification.java
@@ -0,0 +1,18 @@
+package org.onebeartoe.multimedia;
+
+import org.onebeartoe.multimedia.juke.services.RegularCurrentSongService;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RegularCurrentSongServiceSpecification
+{
+    @Test(groups = {"unit"})
+    public void instantiation()
+    {
+        RegularCurrentSongService rcss = new RegularCurrentSongService();
+        
+        
+    }
+}
