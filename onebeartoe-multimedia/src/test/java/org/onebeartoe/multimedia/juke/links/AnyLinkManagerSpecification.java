cd44206db88e6cb280addd7db1a05bbfd76e7dc2
Roberto Marquez <onebeartoe@gmail.com>
2018-10-26 09:21:42 -0500

    More tests were added.

--- onebeartoe-multimedia/src/test/java/org/onebeartoe/multimedia/juke/links/AnyLinkManagerSpecification.java
+++ onebeartoe-multimedia/src/test/java/org/onebeartoe/multimedia/juke/links/AnyLinkManagerSpecification.java
@@ -32,8 +32,38 @@ public class AnyLinkManagerSpecification
     }
     
     @Test(groups = {"unit"})
+    public void add()
+    {
+        implementation.add(null);
+    }
+    
+    @Test(groups = {"unit"})
+    public void clear()
+    {
+        implementation.clear();
+    }
+    
+    @Test(groups = {"unit"})
+    public void getLinsUnitFor()
+    {
+        implementation.getLinksUnitFor("unitName");
+    }
+    
+    @Test(groups = {"unit"})
     public void getLinkUnitNames()
     {
         implementation.getLinkUnitNames();
+    }
+    
+    @Test(groups = {"unit"})
+    public void remove()
+    {
+        implementation.remove(null);
+    }
+    
+    @Test(groups = {"unit"})
+    public void setRootUrl() throws Exception
+    {
+        implementation.setRootUrl("localhost", "fun");
     }
 }
