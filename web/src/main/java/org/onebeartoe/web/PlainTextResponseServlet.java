eecad2da252909fe86045b5383efe674a0474326
onebeartoe <onebeartoe@gmail.com>
2019-05-18 21:14:20 -0500

    whitspace

--- web/src/main/java/org/onebeartoe/web/PlainTextResponseServlet.java
+++ web/src/main/java/org/onebeartoe/web/PlainTextResponseServlet.java
@@ -10,22 +10,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
+ * This servlet sub-class sends plaintext responses.
  */
 public abstract class PlainTextResponseServlet extends HttpServlet
 {
     protected abstract String buildText(HttpServletRequest request, HttpServletResponse response);
-   
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        doResponse(request, response);
+    }
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException 
-    { 
+    {
+        doResponse(request, response);
+    }
+
+    private void doResponse(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
         String text = buildText(request, response);
         
         OutputStream os = response.getOutputStream();
         PrintWriter pw = new PrintWriter(os);
         pw.println(text);
         pw.flush();
-        pw.close();
-    }    
+        pw.close();        
+    }
 }
