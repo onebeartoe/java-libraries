2cad0d2ec67cbb7f6582677d95261e02353bf1a5
roberto <onebeartoe@gmail.com>
2020-04-26 20:41:45 -0500

    add JavaDocs

--- application/src/main/java/org/onebeartoe/application/AppletService.java
+++ application/src/main/java/org/onebeartoe/application/AppletService.java
@@ -5,7 +5,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * @author Roberto Marquez
+ * This class services a default implementation.
  */
 public class AppletService 
 {
