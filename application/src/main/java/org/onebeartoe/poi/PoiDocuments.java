d5a5acc474f1d3d781e793f27eaec20042bd5416
Roberto Marquez <onebeartoe@gmail.com>
2021-03-02 20:59:38 -0700

    add some POI text alignment examples, left and right align on the same line

---
+++ application/src/main/java/org/onebeartoe/poi/PoiDocuments.java
@@ -0,0 +1,77 @@
+
+package org.onebeartoe.poi;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTabStop;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTabs;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STTabJc;
+
+/**
+ * This class provides utility methods for working with MS Word (.docx) documents.
+ */
+public class PoiDocuments 
+{
+    
+    public void edgeAlignedText(XWPFParagraph paragraph, String leftText, String rightText)
+    {
+        XWPFRun tmpRun = paragraph.createRun();
+        tmpRun.setText(leftText);
+        tmpRun.addTab();
+// uncomment for 3 columns            
+//            tmpRun.setText("JAN2");
+//            tmpRun.addTab();
+        tmpRun.setText(rightText);
+// uncomment for 3 columns            
+//            BigInteger pos1 = BigInteger.valueOf(4500);
+//            setTabStop(paragraph, STTabJc.Enum.forString("center"), pos1);
+        BigInteger pos2 = BigInteger.valueOf(9000);
+        setTabStop(paragraph, STTabJc.Enum.forString("right"), pos2);        
+    }
+    
+    public void writeDocument(OutputStream outStream, XWPFDocument document) 
+            throws FileNotFoundException, IOException 
+    {
+        // save the docs
+        
+        document.write(outStream);       
+        
+        outStream.close();
+    }    
+
+    /**
+     * This TabStop allows for two text items on the same line. One is left aligned
+     * and the other is right aligned, on the same line.
+     * 
+     * Code example is from this StackOverflow answer:
+     * 
+     *      https://stackoverflow.com/a/33942296/803890
+     * 
+     * @param oParagraph
+     * @param oSTTabJc
+     * @param oPos 
+     */
+    private void setTabStop(XWPFParagraph oParagraph, STTabJc.Enum oSTTabJc, BigInteger oPos) {
+        CTP oCTP = oParagraph.getCTP();
+        CTPPr oPPr = oCTP.getPPr();
+        if (oPPr == null) {
+            oPPr = oCTP.addNewPPr();
+        }
+
+        CTTabs oTabs = oPPr.getTabs();
+        if (oTabs == null) {
+            oTabs = oPPr.addNewTabs();
+        }
+
+        CTTabStop oTabStop = oTabs.addNewTab();
+        oTabStop.setVal(oSTTabJc);
+        oTabStop.setPos(oPos);
+    }    
+}
---
+++ application/src/test/java/org/onebeartoe/poi/PoiDocumentsSpecification.java
@@ -0,0 +1,78 @@
+
+package org.onebeartoe.poi;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.onebeartoe.poi.PoiDocuments;
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ *
+ */
+public class PoiDocumentsSpecification 
+{
+    private PoiDocuments implementation = new PoiDocuments();
+    
+    @Test
+    public void edgeAlignedText() throws IOException
+    {
+        File outFile = new File("target/aligned.docx");
+        
+        OutputStream outStream = new FileOutputStream(outFile);
+
+        String leftText = "left text poi docs";
+
+        String rightText = "right text poi docs";
+        
+        XWPFDocument document = buildDeftAndRightAlignedTextOnTheSameLine(leftText, rightText);
+        
+        implementation.writeDocument(outStream, document);
+        
+        assertTrue( outFile.exists() );
+        
+        assertTrue( outFile.length() > 0);
+        
+        System.out.println("document path = " + outFile.getAbsolutePath() );
+        
+        XWPFDocument filesystemDocument = new XWPFDocument(
+                Files.newInputStream( outFile.toPath() ) );
+        
+        XWPFWordExtractor xwpfWordExtractor = new XWPFWordExtractor(filesystemDocument);
+            
+        String docText = xwpfWordExtractor.getText();
+        
+        System.out.println("docText = " + docText);
+
+        assertTrue( docText.contains(leftText) );
+        
+        assertTrue( docText.contains(rightText) );
+                   
+        String actual = docText.replace(leftText, "");        
+        System.out.println("acutal 1 = " + actual);
+        
+        actual = actual.replace(rightText, "");                
+        System.out.println("acutal 2 = " + actual);
+        
+        assertTrue(actual.trim().isEmpty() );       
+    }
+    
+    private XWPFDocument buildDeftAndRightAlignedTextOnTheSameLine(String leftText, String rightText)
+    {
+        XWPFDocument doc = new XWPFDocument();
+        
+        // This version uses TabStops to have two text items on the same line, but
+        // with separate left and right alignment.
+        XWPFParagraph paragraph = doc.createParagraph();
+
+        implementation.edgeAlignedText(paragraph, leftText, rightText);
+        
+        return doc;
+    }
+}
